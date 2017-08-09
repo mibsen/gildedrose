819f20a320b31e2f536cf354c7a06361caef4a0b
mibsen <martinribsen@gmail.com>
2017-08-09 12:11:06 +0200

    Added unit tests and added Conjured functionality

--- src/main/java/net/praqma/codeacademy/gildedrose/GildedRose.java
+++ src/main/java/net/praqma/codeacademy/gildedrose/GildedRose.java
@@ -6,15 +6,25 @@ class GildedRose {
     public GildedRose(Item[] items) {
         this.items = items;
     }
-
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
+
+            int degradation = 1;
+
+            if (items[i].name.contains("Conjured")){
+                degradation = 2;
+            }
+
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
+                        if(items[i].quality - (1 * degradation) > 0){
+                            items[i].quality = items[i].quality - (1 * degradation);
+                        } else {
+                           items[i].quality = 0;
+                         }
+                       } 
                 }
             } else {
                 if (items[i].quality < 50) {
@@ -45,11 +55,11 @@ class GildedRose {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].quality > 0) {
                             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
+                                items[i].quality = items[i].quality  - (1 * degradation);
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        items[i].quality = items[i].quality - (items[i].quality * degradation);
                     }
                 } else {
                     if (items[i].quality < 50) {
